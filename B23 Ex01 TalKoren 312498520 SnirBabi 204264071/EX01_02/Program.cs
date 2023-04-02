68b388ea475716c7635392381ee329d14e7e6c22
Tal Koren <talko4245@gmail.com>
2023-04-02 13:03:24 +0300

    Finish code section

---
+++ B23 Ex01 TalKoren 312498520 SnirBabi 204264071/EX01_02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EX01_02
+{
+    public class Program
+    {
+        private const int k_diamnodHight = 9;
+
+        public static void Main()
+        {
+            PrintDiamond(k_diamnodHight);
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadLine();
+        }
+
+        public static void PrintDiamond(int i_dianmondHight, int i_currentRow = 1)
+        {
+            if (i_dianmondHight % 2 == 0)
+            {
+                i_dianmondHight--;
+            }
+
+            int numberOfStarsToPrint = i_currentRow + (i_currentRow - 1);
+            int numberOfSpacesToPrint = (i_dianmondHight - numberOfStarsToPrint) / 2;
+            printChararcterForLength(' ', numberOfSpacesToPrint);
+            printChararcterForLength('*', numberOfStarsToPrint);
+            Console.WriteLine();
+            if (i_currentRow * 2 > i_dianmondHight)
+            {
+                return;
+            }
+            else
+            {
+                PrintDiamond(i_dianmondHight, ++i_currentRow);
+            }
+
+            printChararcterForLength(' ', numberOfSpacesToPrint);
+            printChararcterForLength('*', numberOfStarsToPrint);
+            Console.WriteLine();
+        }
+
+        private static void printChararcterForLength(char i_character, int i_length)
+        {
+            for (int i = 0; i < i_length; ++i)
+            {
+                Console.Write(i_character);
+            }
+        }
+    }
+}
